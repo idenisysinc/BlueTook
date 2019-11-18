730a707af80363e7542d136c290bb23e0cc9a38d
Amit Sharma <amit@digeratiwebcrafts.com>
2019-11-18 17:45:15 +0530

    Partner Profile Public UI Design

---
+++ partner-profile-public.php
@@ -0,0 +1,328 @@
+
+	<?php include('header.php') ?>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content bg-white">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+			<div class="profile-public">
+				<div class="bg-img-overlay"></div>
+			</div>
+			<!-- Content area -->
+			<div class="content profile-public-ctn">
+				<div class="row">
+					<div class="col-lg-9">
+						<div>
+							<div class="card-body mob-text-center">
+								<div class="card-img-actions d-inline-block mb-3 pro-img-border float-md-left mr-md-4">
+									<img class="img-fluid rounded-circle" src="assets/images/placeholders/placeholder.jpg" width="190" height="190" alt="">
+									<div class="card-img-actions-overlay card-img rounded-circle">
+										<button id="videoDetailsBtn" class="btn btn-outline bg-white text-white border-white border-2 btn-icon rounded-round" title="Profile">
+											<i class="icon-play4"></i>
+										</button>
+									</div>
+								</div>
+
+					    		<div class="float-md-left mt-md-4">
+					    			<div class="mb-4 text-white">
+					    				<h6 class="font-weight-bold mb-0 mob-dark-text">James Alexander</h6>
+										<span class="opacity-50 mob-dark-text">City, State</span>
+										<p><i class="icon-star-full2 font-size-sm star-rate-on"></i> <i class="icon-star-full2 font-size-sm star-rate-on"></i> <i class="icon-star-full2 font-size-sm star-rate-on"></i> <i class="icon-star-full2 font-size-sm star-rate-off"></i> <i class="icon-star-full2 font-size-sm star-rate-off"></i></p>
+					    			</div>
+									<button class="openmodal-cust myBtn link-btn">Achievements</button> <span>&nbsp;|&nbsp;</span>
+									<button class="openmodal-cust myBtn link-btn">Reviews</button> <span>&nbsp;|&nbsp;</span>
+									<button class="openmodal-cust myBtn link-btn">Projects</button>
+					    		</div>
+					    	</div>
+				    	</div>
+					</div>
+					<div class="col-lg-3">
+						<div class="px-4 text-white">
+							<p class="text-lgt-blue mb-0 mt-3 tab-dark-text">Starting From</p>
+							<h4 class="tab-dark-text">Rs. 12000</h4>
+							<button type="button" id="viewDetailsBtn" class="btn btn-light d-block w-100 text-dp-blue">View Details <i class="icon-arrow-right7 ml-1"></i></button>
+							<div class="mt-4">
+								<button type="button" class="btn btn-primary book-now-btn gradi-btn">Book Now <i class="icon-camera ml-1"></i></button><button type="button" class="btn btn-primary chat-now-btn gradi-btn">Chat Now <i class="icon-bubbles2 ml-1"></i></button>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Trigger/Open The Modal -->
+			</div>
+			<!-- /Content area -->
+			<!-- Content area -->
+			<div class="content">
+				<div class="card radius-10 custom-box-shadow bg-light">
+					<div class="row py-4 px-md-4">
+						<div class="col-sm-6">
+							<div class="card-body">
+								<h5><strong><i class="icon-user mr-1 link-color font-size-25"></i> About me</strong></h5>
+								<p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <a href="#" class="link-color">[Read More]</a></p>
+							</div>
+						</div>
+						<div class="col-sm-6">
+							<div class="card-body">
+								<h5><strong><i class="icon-medal mr-1 link-color font-size-25"></i> Award &amp; Achievement</strong></h5>
+								<p class="mb-0">Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum. <a href="#" class="link-color">[Read More]</a></p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- Image grid -->
+				<div class="text-center mt-5 mb-3">
+					<a href="#" class="tab-filter active">Photos</a>
+					<a href="#" class="tab-filter">Weddings</a>
+					<a href="#" class="tab-filter">Photo Shoots</a>
+					<a href="#" class="tab-filter">Weddings</a>
+					<a href="#" class="tab-filter">Photo Shoots</a>
+					<a href="#" class="tab-filter">Weddings</a>
+					<a href="#" class="tab-filter">Photo Shoots</a>
+				</div>
+				<div class="row">
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+				</div>
+
+				<div class="row">
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+
+					<div class="col-sm-6 col-lg-2">
+						<div class="card">
+							<div class="card-img-actions m-1">
+								<img class="card-img img-fluid" src="assets/images/placeholders/placeholder.jpg" alt="">
+							</div>
+						</div>
+					</div>
+					
+				</div>
+				<!-- /Image grid -->
+			</div>
+			<!-- /content area -->
+
+
+			<!-- Footer -->
+			<?php include('footer.php') ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+	<!-- The Modal -->
+	<div class="modal-cust">
+
+	  <!-- Modal content -->
+	  <div class="modal-content-cust">
+	    <span class="close-cust">&times;</span>
+	    <div class="p-3">
+    		<p>Some text in the Modal..</p>
+		</div>
+	  </div>
+
+	</div>
+	<div class="modal-cust">
+
+	  <!-- Modal content -->
+	  <div class="modal-content-cust">
+	    <span class="close-cust">&times;</span>
+	    <div class="p-3">
+    		<p>Some text in the Modal..</p>
+		</div>
+	  </div>
+
+	</div>
+	<div class="modal-cust">
+
+	  <!-- Modal content -->
+	  <div class="modal-content-cust">
+	    <span class="close-cust">&times;</span>
+    	<div class="p-3">
+    		<p>Some text in the Modal..</p>
+		</div>
+	  </div>
+
+	</div>
+	<!-- /The Modal -->
+	<!-- View Details Modal -->
+	<div>
+		<!-- The Modal -->
+		<div id="viewDetailsModal">
+
+		  <!-- Modal content -->
+		  <div class="modal-content-cust viewDetailsContent">
+		    <span class="close-view-details">&times;</span>
+		    <div class="p-3 text-content">
+		    	<p><i class="icon-calendar3 link-color mr-2"></i><strong>Event name</strong> - Wedding, birthday, baby shoot etc</p>
+				<p><i class="icon-price-tags link-color mr-1"></i> <strong>Rate</strong> - Price in rupees</p>
+				<p><i class="icon-checkmark link-color mr-2"></i><strong>Rate type</strong> - eg hourly, daily etc</p>
+				<p><i class="icon-file-text link-color mr-2"></i><strong>Notes</strong> - a short note upto 150 chars</p>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+				<p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+		    </div>
+		  </div>
+
+		</div>
+	</div>
+	<!-- View Details Modal -->
+	<!-- Video Modal -->
+	<div>
+		<!-- The Modal -->
+		<div id="videoDetailsModal">
+
+		  	<!-- Modal content -->
+		  	<div class="modal-content-cust">
+		    	<span class="close-video">&times;</span>
+		    	<div class="p-3">
+		    		<p>This is the video popup.</p>
+		    	</div>
+		  	</div>
+		</div>
+	</div>
+	<!-- Video Modal -->
+	<!-- script for achievements, reviews, projects -->
+	<script>
+		var modals = document.getElementsByClassName('modal-cust');
+		// Get the button that opens the modal
+		var btns = document.getElementsByClassName("openmodal-cust");
+		var spans=document.getElementsByClassName("close-cust");
+		for(let i=0;i<btns.length;i++){
+		    btns[i].onclick = function() {
+		        modals[i].style.display = "block";
+		    }
+		}
+		for(let i=0;i<spans.length;i++){
+		    spans[i].onclick = function() {
+		        modals[i].style.display = "none";
+		    }
+		}
+	</script>
+	<!-- script for view details -->
+	<script>
+		// Get the modal
+		var modal_1 = document.getElementById("viewDetailsModal");
+		var modal_2 = document.getElementById("videoDetailsModal");
+		// Get the button that opens the modal
+		var btn_1 = document.getElementById("viewDetailsBtn");
+		var btn_2 = document.getElementById("videoDetailsBtn");
+		// Get the <span> element that closes the modal
+		var span_1 = document.getElementsByClassName("close-view-details")[0];
+		var span_2 = document.getElementsByClassName("close-video")[0];
+
+		// When the user clicks the button, open the modal 
+		btn_1.onclick = function() {
+		  modal_1.style.display = "block";
+		}
+		btn_2.onclick = function() {
+		  modal_2.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span_1.onclick = function() {
+		  modal_1.style.display = "none";
+		}
+		span_2.onclick = function() {
+		  modal_2.style.display = "none";
+		}
+
+		// When the user clicks anywhere outside of the modal, close it
+		window.onclick = function(event_1) {
+		  if (event_1.target == modal_1) {
+		    modal_1.style.display = "none";
+		  }
+		}
+		window.onclick = function(event_2) {
+		  if (event_2.target == modal_2) {
+		    modal_2.style.display = "none";
+		  }
+		}
+		</script>
+</body>
+</html>
