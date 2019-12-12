e2af0806bd67ec4f5faa7066d3e37bf4ed6c3adb
Amit Sharma <amit@digeratiwebcrafts.com>
2019-12-12 13:51:35 +0530

    Subscription  UI

---
+++ subscription.php
@@ -0,0 +1,80 @@
+
+	<?php include('header.php') ?>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content bg-white">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+			<div class="cover-bg-div position-relative">
+				<div class="bg-img-overlay"></div>
+				<img src="assets/images/subscription-banner.jpg" alt="BG Image" class="img-fluid bg-image subs-img">
+				<div class="content w-90 m-auto text-center text-ctn">
+					<div class="text-ctn-details">
+						<h1 class="position-relative title title-before title-after">Subscription Plan Details</h1>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Content area -->
+			<div class="content w-90 w-md-75 m-auto">
+				<!-- Image grid -->
+				<div class="text-center mt-4 mb-3">
+					<a href="#" class="tab-filter active">Package Name</a>
+					<a href="#" class="tab-filter">Package Name Lorem</a>
+					<a href="#" class="tab-filter">Package Name Lorem</a>
+					<a href="#" class="tab-filter">Package</a>
+				</div>
+				<div class="row my-4">
+					<div class="col-lg-4 d-flex flex-wrap align-content-center pr-lg-4 pr-xl-5 border-rt1">
+						<h1 class="blk-lgt-color">Package Name</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Praesent rutrum justo ut arcu euismod, ac.</p>
+						<div class="link-color">
+							<h5 class="mb-0 price-dtls"><strong>Per Month</strong></h5>
+							<p class="font-25 price-dtls font-40"><strong>₹ 5,000</strong></p>
+							<button type="button" class="btn btn-primary gradi-btn px-4 d-block w-100">Subscribe Now</button>
+						</div>
+					</div>
+					<div class="col-lg-8 d-flex flex-wrap align-content-center pl-lg-4 pl-xl-5">
+						<ul class="external-bullet tab-mt20">
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, esse debitis quam illum soluta sapiente eligendi maiores reiciendis magni perspiciatis vitae deleniti! Suscipit, modi, eos.</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, esse debitis quam illum soluta sapiente eligendi maiores reiciendis magni perspiciatis vitae deleniti! Suscipit, modi, eos.</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, </li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, esse debitis quam illum soluta sapiente eligendi maiores reiciendis magni perspiciatis vitae deleniti! Suscipit, modi, eos.</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur </li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, esse debitis quam illum soluta sapiente eligendi maiores reiciendis magni perspiciatis vitae deleniti! Suscipit, modi, eos.</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur </li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</li>
+							<li>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cumque consequatur id assumenda, delectus temporibus, esse debitis quam illum soluta sapiente eligendi maiores reiciendis magni perspiciatis vitae deleniti! Suscipit, modi, eos.</li>
+						</ul>
+					</div>
+				</div>
+			</div>
+			<!-- /content area -->
+			<div class="position-relative divider-bg-div" style="background-image: url(assets/images/subscription-section-banner.jpg);background-repeat: no-repeat;background-position: center;background-size: cover;">
+			<div class="bg-img-overlay"></div>
+				<div class="content w-90 m-auto text-center text-white py-4">
+					<h2 class="position-relative mb-0">Benefits of the package</h2>
+					<p class="position-relative w-60 mob-w-100 m-auto">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Proin semper magna in est elementum, vitae semper turpis gravida.
+	Pellentesque a justo odio. Phasellus aliquam nunc volutpat, sagittis ex dignissim, mattis.</p>
+				</div>
+			</div>
+
+			<!-- Sub Footer -->
+			<?php include('sub-footer.php') ?>
+			<!-- / Subfooter -->
+			<!-- Footer -->
+			<?php include('footer.php') ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+	
+</body>
+</html>
