be5d1220ae4ee7498d4b4be667728e0627f0ebe6
Amit Sharma <amit@digeratiwebcrafts.com>
2019-12-30 19:40:28 +0530

    Contact Us and About Bluetook UI

---
+++ about-bluetook.php
@@ -0,0 +1,79 @@
+
+	<?php include('header.php') ?>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content bg-white">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+			<div class="cover-bg-div position-relative">
+				<div class="bg-img-overlay"></div>
+				<img src="assets/images/aboutus-banner.jpg" alt="BG Image" class="img-fluid bg-image subs-img">
+				<div class="content w-90 m-auto text-center text-ctn">
+					<div class="text-ctn-details">
+						<h1 class="position-relative title title-before title-after">About Bluetook</h1>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Content area -->
+			<div class="content w-90 w-md-75 m-auto">
+				<div class="row my-4">
+					<div class="col-lg-8 d-flex flex-column justify-content-center pr-lg-4">
+						<h2 class="blk-lgt-color cust-line-ht">Who We Are And Why We Do What We Do?</h2>
+						<p class="text-justify">Here’s the thing about life: it changes, and so does our memory- even when you want to hold on to precious moments you can’t do it for long. We are, frankly, blown away by this fact and wish to capture the best moments through our art.</p>
+
+						<p class="mb-0 text-justify">Bluetook is a land of tales and tellers of the finest kind, a creative community where you can connect with the right creative artists- photographers, cinematographers, models, editors, make-up artists and the likes so when your story meets our storyteller, the tale is simply stunning.</p>
+					</div>
+					<div class="col-lg-4 d-flex flex-column justify-content-center sm-view-mt">
+						<img src="assets/images/aboutus-body-01.jpg" class="img-fluid full-radius5 custom-box-shadow" alt="">
+					</div>
+				</div>
+				<!-- desk view -->
+				<div class="row my-4 desk-view">
+					<div class="col-lg-4 d-flex flex-column justify-content-center">
+						<img src="assets/images/aboutus-body-02.jpg" class="img-fluid full-radius5 custom-box-shadow" alt="">
+						
+					</div>
+					<div class="col-lg-8 d-flex flex-column justify-content-center pl-lg-4">
+						<h2 class="blk-lgt-color cust-line-ht">Explore The Whimsical Tales/Memories Of The Past</h2>
+						<p class="text-justify">Here’s a little sampling of how moments are captured and created into memories and when you’ve been doing it for years, you’re likely to develop quite a collection.</p>
+						<h2 class="blk-lgt-color mt-3">Tales/Memories Yet To Be Created</h2>
+						<p class="mb-0 text-justify">We know you have plenty of memories that are yet to be created, and- as luck would have it– we have plenty of creators waiting to create them. Here are just a few of the creators you’ll discover while you’re here.</p>
+					</div>
+				</div>
+				<!-- /desk view -->
+				<!-- mobile view -->
+				<div class="row my-4 mob-view">
+					<div class="col-lg-8 d-flex flex-column justify-content-center pl-lg-4">
+						<h2 class="blk-lgt-color cust-line-ht">Explore The Whimsical Tales/Memories Of The Past</h2>
+						<p class="text-justify">Here’s a little sampling of how moments are captured and created into memories and when you’ve been doing it for years, you’re likely to develop quite a collection.</p>
+						<h2 class="blk-lgt-color mt-3 cust-line-ht">Tales/Memories Yet To Be Created</h2>
+						<p class="mb-0 text-justify">We know you have plenty of memories that are yet to be created, and- as luck would have it– we have plenty of creators waiting to create them. Here are just a few of the creators you’ll discover while you’re here.</p>
+					</div>
+					<div class="col-lg-4 d-flex flex-column justify-content-center">
+						<img src="assets/images/aboutus-body-02.jpg" class="img-fluid full-radius5 custom-box-shadow sm-view-mt" alt="">
+						
+					</div>
+				</div>
+				<!-- /mobile view -->
+			</div>
+			<!-- /content area -->
+
+			<!-- Sub Footer -->
+			<?php include('sub-footer.php') ?>
+			<!-- / Subfooter -->
+			<!-- Footer -->
+			<?php include('footer.php') ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+	
+</body>
+</html>
---
+++ contact-us.php
@@ -0,0 +1,132 @@
+
+	<?php include('header.php') ?>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content bg-white">
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+			<div class="cover-bg-div position-relative">
+				<div class="bg-img-overlay"></div>
+				<img src="assets/images/contactus-banner.jpg" alt="BG Image" class="img-fluid bg-image subs-img">
+				<div class="content w-90 m-auto text-center text-ctn">
+					<div class="text-ctn-details">
+						<h1 class="position-relative title title-before title-after">Contact Us</h1>
+					</div>
+				</div>
+			</div>
+			
+			<!-- Content area -->
+			<div class="content w-90 w-md-75 m-auto">
+				<div class="row my-4">
+					<div class="col-lg-5 d-flex flex-column justify-content-center">
+						<div class="row mb-3">
+							<div class="col-sm-12">
+								<img src="assets/images/bluetook-logo-new.jpg" class="img-fluid" alt="Logo">
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col-sm-3">
+								<p class="mb-0"><strong>Address:</strong></p>
+							</div>
+							<div class="col-sm-9">
+								<p class="mb-0">Akshya Nagar 1st Block 1st Cross, Rammurthy nagar, Bangalore-560016</p>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col-sm-3">
+								<p class="mb-0"><strong>Call Us:</strong></p>
+							</div>
+							<div class="col-sm-9">
+								<p class="mb-0"><a href="tel:+919876543210">+91 9876543210</a> / <a href="tel:+919876543210">+91 9876543210</a></p>
+							</div>
+						</div>
+						<div class="row mb-2">
+							<div class="col-sm-3">
+								<p class="mb-0"><strong>Email Us:</strong></p>
+							</div>
+							<div class="col-sm-9">
+								<p class="mb-0"><a href="mailto:hi@bluetook.com">hi@bluetook.com</a></p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-sm-3">
+								<p class="mb-0"><strong>Follow Us:</strong></p>
+							</div>
+							<div class="col-sm-9">
+								<a href="#" target="_blank"><img src="assets/images/facebook.jpg" class="img-fluid mr-2" alt="facebook"></a>
+								<a href="#" target="_blank"><img src="assets/images/insta.jpg" class="img-fluid" alt="Insta"></a>
+							</div>
+						</div>
+					</div>
+					<div class="col-lg-7 d-flex flex-column justify-content-center">
+						<form action="">
+							<div class="custom-box-shadow full-radius5 p-2 contc-details">
+								<div class="card-body">
+									<div class="row">
+										<div class="col-sm-12">
+											<h2 class="font-25 blk-lgt-color">Contact Us</h2>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12">
+											<div class="form-group">
+												<input type="text" class="form-control" placeholder="Your Name" required="">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-6">
+											<div class="form-group">
+												<input type="text" class="form-control" placeholder="Phone No." required="">
+											</div>
+										</div>
+										<div class="col-sm-6">
+											<div class="form-group">
+												<input type="email" class="form-control" placeholder="Email Id" required="">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12">
+											<div class="form-group">
+												<input type="text" class="form-control" placeholder="Subject" required="">
+											</div>
+										</div>
+									</div>
+									<div class="row">
+										<div class="col-sm-12">
+											<div class="form-group">
+												<textarea cols="30" rows="5" class="form-control" placeholder="Description" required=""></textarea>
+											</div>
+										</div>
+									</div>
+									<button type="submit" class="btn bg-indigo-400 gradi-btn"><i class="icon-paperplane mr-2"></i> Submit</button>
+								</div>
+							</div>
+						</form>
+					</div>
+				</div>
+			</div>
+			<!-- /content area -->
+			<div class="map-details">
+				<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d28035.61027590381!2d77.08244814488363!3d28.556208360268933!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390d1c4005d013a7%3A0x1a9ac813cd3cd60f!2sCentral%20Bank%20of%20India!5e0!3m2!1sen!2sin!4v1577711220210!5m2!1sen!2sin" height="250" frameborder="0" style="border:0;" allowfullscreen=""></iframe>
+			</div>
+
+			<!-- Sub Footer -->
+			<?php include('sub-footer.php') ?>
+			<!-- / Subfooter -->
+			<!-- Footer -->
+			<?php include('footer.php') ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+	
+</body>
+</html>
