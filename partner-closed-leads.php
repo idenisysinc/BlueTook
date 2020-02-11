8956a90f3fcf7529a4fb00a5976c79d3b0083021
Amit Sharma <amit@digeratiwebcrafts.com>
2020-02-11 17:05:55 +0530

    Partner Responded Leads, Missed Leads, Closed Leads UI

---
+++ partner-closed-leads.php
@@ -0,0 +1,228 @@
+
+	<?php include('header.php') ?>
+	<!-- /main navbar -->
+
+
+	<!-- Page content -->
+	<div class="page-content">
+
+		<!-- Main sidebar -->
+		<?php include('partner-main-sidebar.php') ?>
+		<!-- /main sidebar -->
+
+
+		<!-- Main content -->
+		<div class="content-wrapper">
+
+			<!-- Page header -->
+			<div class="page-header page-header-light">
+				<div class="page-header-content header-elements-md-inline">
+					<div class="page-title d-flex">
+						<h4 class="font-weight-semibold"><i class="icon-users4 mr-2"></i>Closed Leads</h4>
+					</div>
+				</div>
+
+				<div class="breadcrumb-line breadcrumb-line-light header-elements-md-inline">
+					<div class="d-flex">
+						<div class="breadcrumb">
+							<a href="#" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
+							<span class="breadcrumb-item active">Closed Leads</span>
+						</div>
+					</div>
+
+				</div>
+			</div>
+			<!-- /page header -->
+
+
+			<!-- Content area -->
+			<div class="content">
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-2 text-xl-center">
+									<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="70" height="70" alt="">
+									<p class="m-0 font-weight-semibold">Swati Raj</p>
+									<p class="m-0 text-secondary">Kolkata</p>
+									<p class="mb-2 mb-xl-0"><span class="badge badge-pill bg-grey px-3">Closed</span></p>
+								</div>
+								<div class="col-xl-10">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Photography - Wedding <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-secondary m-0 clear-both">
+											Budget: <span>Rs. 50000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-xl-12 d-flex align-items-center">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-2 text-xl-center">
+									<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="70" height="70" alt="">
+									<p class="m-0 font-weight-semibold">Swati Raj</p>
+									<p class="m-0 text-secondary">Kolkata</p>
+									<p class="mb-2 mb-xl-0"><span class="badge badge-pill bg-grey px-3">Closed</span></p>
+								</div>
+								<div class="col-xl-10">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Videography - Drone <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-secondary m-0 clear-both">
+											Budget: <span>Rs. 50000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-xl-12 d-flex align-items-center">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-2 text-xl-center">
+									<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="70" height="70" alt="">
+									<p class="m-0 font-weight-semibold">Swati Raj</p>
+									<p class="m-0 text-secondary">Kolkata</p>
+									<p class="mb-2 mb-xl-0"><span class="badge badge-pill bg-grey px-3">Closed</span></p>
+								</div>
+								<div class="col-xl-10">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Editing - High End <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-secondary m-0 clear-both">
+											Budget: <span>Rs. 50000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-xl-12 d-flex align-items-center">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- pagination -->
+				<div class="w-80 m-auto full-width-mob">
+					<ul class="pagination pagination-flat justify-content-center">
+						<li class="page-item"><a href="#" class="page-link">&larr; &nbsp; Prev</a></li>
+						<li class="page-item active"><a href="#" class="page-link">1</a></li>
+						<li class="page-item"><a href="#" class="page-link">2</a></li>
+						<li class="page-item disabled"><a href="#" class="page-link">3</a></li>
+						<li class="page-item"><a href="#" class="page-link">4</a></li>
+						<li class="page-item"><a href="#" class="page-link">Next &nbsp; &rarr;</a></li>
+					</ul>
+				</div>
+				<!-- /pagination -->
+			</div>
+			<!-- /content area -->
+
+
+			<!-- Footer -->
+			<?php include('footer.php') ?>
+			<!-- /footer -->
+
+		</div>
+		<!-- /main content -->
+
+	</div>
+	<!-- /page content -->
+	<!-- Large modal -->
+		<div id="modal_large" class="modal fade" tabindex="-1">
+			<div class="modal-dialog modal-lg">
+				<form action="#">
+					<div class="modal-content">
+						<div class="modal-header grey-bg text-white pb-3">
+							<h1 class="modal-title">Make an Offer: <span>Photography - Wedding</span></h1>
+							<button type="button" class="close" data-dismiss="modal">&times;</button>
+						</div>
+
+						<div class="modal-body">
+							<div class="row">
+								<div class="col-lg-6">
+									<div class="form-group">
+										<label class="d-block"><strong>Bid Amount</strong></label>
+										<input type="text" class="form-control" placeholder="Enter Amount">
+									</div>
+								</div>
+								<div class="col-lg-6">
+									<div class="form-group">
+										<label class="d-block"><strong>Delivery Notes</strong> (Max 90 chars)</label>
+										<input type="text" class="form-control" placeholder="eg. 100 Photos or 1hr video">
+									</div>
+									<!-- <div class="card mb-0">
+										<div class="card-body text-center">
+											<h5 class="font-weight-semibold">Collaborate and Bid</h5>
+											<div>
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+											</div>
+											<div>
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+												<img src="assets/images/placeholders/placeholder.jpg" class="rounded-circle mb-1" width="44" height="44" alt="">
+											</div>
+											<p class="mt-2 mb-0 text-muted">Select Partners who will join you in the project</p>
+										</div>
+									</div> -->
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-12">
+									<div class="form-group">
+										<label class="d-block"><strong>Description</strong> (Max 2000 chars)</label>
+										<textarea rows="5" cols="5" class="form-control" placeholder="Enter your message here"></textarea>
+									</div>
+								</div>
+							</div>
+						</div>
+
+						<div class="card-footer">
+							<button type="button" class="btn bg-grey">Submit <i class="icon-circle-right2 ml-2"></i> </button>
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	<!-- /large modal -->
+
+</body>
+</html>
