1073f56653c9a0fc7629fc35a339f9f4d4cd07e5
Amit Sharma <amit@digeratiwebcrafts.com>
2020-02-11 15:30:02 +0530

    Partner New Lead UI Update

--- partner-lead-response.php
+++ partner-lead-response.php
@@ -18,7 +18,7 @@
 			<div class="page-header page-header-light">
 				<div class="page-header-content header-elements-md-inline">
 					<div class="page-title d-flex">
-						<h4 class="font-weight-semibold"><i class="icon-users4 mr-2"></i>Leads</h4>
+						<h4 class="font-weight-semibold"><i class="icon-users4 mr-2"></i>New Leads</h4>
 					</div>
 				</div>
 
@@ -26,7 +26,7 @@
 					<div class="d-flex">
 						<div class="breadcrumb">
 							<a href="#" class="breadcrumb-item"><i class="icon-home2 mr-2"></i> Home</a>
-							<span class="breadcrumb-item active">Leads</span>
+							<span class="breadcrumb-item active">New Leads</span>
 						</div>
 					</div>
 
@@ -38,7 +38,7 @@
 			<!-- Content area -->
 			<div class="content">
 				<!-- row-->
-				<div class="row">
+				<!-- <div class="row">
 					<div class="col-lg-6">
 						<div class="card">
 							<div class="card-body">
@@ -75,9 +75,9 @@
 								<button type="button" class="btn btn-light ml-3" data-toggle="modal" data-target="#modal_large"><i class="icon-reply-all mr-2"></i> Respond</button>
 							</div>
 						</div>
-						<!-- /.card -->
+			
 					</div>
-					<!-- /col-lg-6 -->
+				
 					<div class="col-lg-6">
 						<div class="card">
 							<div class="card-body">
@@ -114,10 +114,9 @@
 								<button type="button" class="btn btn-light ml-3" data-toggle="modal" data-target="#modal_large"><i class="icon-reply-all mr-2"></i> Respond</button>
 							</div>
 						</div>
-						<!-- /.card -->
+					
 					</div>
-					<!-- /col-lg-6 -->
-					<!-- pagination -->
+					
 					<div class="w-100">
 						<ul class="pagination pagination-flat align-self-center float-right">
 							<li class="page-item"><a href="#" class="page-link">&larr; &nbsp; Prev</a></li>
@@ -128,9 +127,141 @@
 							<li class="page-item"><a href="#" class="page-link">Next &nbsp; &rarr;</a></li>
 						</ul>
 					</div>
-					<!-- /pagination -->
-				</div>
+					
+				</div> -->
 				<!-- /row-->
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-4">
+									<div class="card-img-actions d-inline-block mb-2 mb-xl-0">
+										<img src="assets/images/client-album-cover.jpg" class="rounded" width="240" height="135" alt="">
+										<div class="card-img-actions-overlay card-img overlay-img-text">
+											Photography
+										</div>
+									</div>
+								</div>
+								<div class="col-xl-8">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Wedding <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-muted font-weight-normal m-0 clear-both"><i class="icon-pin font-size-sm "></i> City, State</p>
+										<p class="text-secondary">
+											Budget: <span>Rs. 50000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-lg-8  d-flex align-items-center tab-mb-10">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+								<div class="col-lg-4 text-lg-right">
+									<button type="button" class="btn bg-grey" data-toggle="modal" data-target="#modal_large">Bid Now</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-4">
+									<div class="card-img-actions d-inline-block mb-2 mb-xl-0">
+										<img src="assets/images/client-album-cover.jpg" class="rounded" width="240" height="135" alt="">
+										<div class="card-img-actions-overlay card-img overlay-img-text">
+											Videography
+										</div>
+									</div>
+								</div>
+								<div class="col-xl-8">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Videography <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-muted font-weight-normal m-0 clear-both"><i class="icon-pin font-size-sm "></i> City, State</p>
+										<p class="text-secondary">
+											Budget: <span>Rs. 60000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-lg-8  d-flex align-items-center tab-mb-10">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+								<div class="col-lg-4 text-lg-right">
+									<button type="button" class="btn bg-grey" data-toggle="modal" data-target="#modal_large">Bid Now</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="w-80 m-auto full-width-mob">
+					<div class="card">
+						<div class="card-body">
+							<div class="row">
+								<div class="col-xl-4">
+									<div class="card-img-actions d-inline-block mb-2 mb-xl-0">
+										<img src="assets/images/client-album-cover.jpg" class="rounded" width="240" height="135" alt="">
+										<div class="card-img-actions-overlay card-img overlay-img-text">
+											Editing
+										</div>
+									</div>
+								</div>
+								<div class="col-xl-8">
+									<div class="media-body">
+										<div>
+											<h6 class="media-title font-weight-semibold float-lg-left">Editing <span> | 22-Feb-2020</span></h6>
+											<p class="m-0 float-lg-right">Posted: <span>10 days ago</span></p>
+										</div>
+										<p class="text-muted font-weight-normal m-0 clear-both"><i class="icon-pin font-size-sm "></i> City, State</p>
+										<p class="text-secondary">
+											Budget: <span>Rs. 20000</span>
+										</p>
+										<p class="text-justify">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Eligendi molestias consequuntur incidunt, ab accusantium numquam, temporibus. Deserunt consectetur fugit nam rem doloribus temporibus repellendus, ut fugiat sit molestiae id omnis.</p>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="card-footer">
+							<div class="row">
+								<div class="col-lg-8  d-flex align-items-center tab-mb-10">
+									<p class="float-left m-0 mr-2">10 Responses</p>
+									<p class="float-left m-0">Avg Bid: Rs. 48700</p>
+								</div>
+								<div class="col-lg-4 text-lg-right">
+									<button type="button" class="btn bg-grey" data-toggle="modal" data-target="#modal_large">Bid Now</button>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<!-- pagination -->
+				<div class="w-80 m-auto full-width-mob">
+					<ul class="pagination pagination-flat justify-content-center">
+						<li class="page-item"><a href="#" class="page-link">&larr; &nbsp; Prev</a></li>
+						<li class="page-item active"><a href="#" class="page-link">1</a></li>
+						<li class="page-item"><a href="#" class="page-link">2</a></li>
+						<li class="page-item disabled"><a href="#" class="page-link">3</a></li>
+						<li class="page-item"><a href="#" class="page-link">4</a></li>
+						<li class="page-item"><a href="#" class="page-link">Next &nbsp; &rarr;</a></li>
+					</ul>
+				</div>
+				<!-- /pagination -->
 			</div>
 			<!-- /content area -->
 
@@ -150,24 +281,24 @@
 				<form action="#">
 					<div class="modal-content">
 						<div class="modal-header grey-bg text-white pb-3">
-							<h1 class="modal-title">POST YOUR BID</h1>
+							<h1 class="modal-title">Make an Offer: <span>Photography - Wedding</span></h1>
 							<button type="button" class="close" data-dismiss="modal">&times;</button>
 						</div>
 
 						<div class="modal-body">
 							<div class="row">
-								<div class="col-lg-8">
+								<div class="col-lg-6">
 									<div class="form-group">
-										<label class="d-block"><strong>Description</strong></label>
-										<textarea rows="5" cols="5" class="form-control" placeholder="Enter your message here"></textarea>
-									</div>
-									<div class="form-group">
-										<label class="d-block"><strong>Budget</strong></label>
-										<input type="text" class="form-control" placeholder="Budget">
+										<label class="d-block"><strong>Bid Amount</strong></label>
+										<input type="text" class="form-control" placeholder="Enter Amount">
 									</div>
 								</div>
-								<div class="col-lg-4">
-									<div class="card mb-0">
+								<div class="col-lg-6">
+									<div class="form-group">
+										<label class="d-block"><strong>Delivery Notes</strong> (Max 90 chars)</label>
+										<input type="text" class="form-control" placeholder="eg. 100 Photos or 1hr video">
+									</div>
+									<!-- <div class="card mb-0">
 										<div class="card-body text-center">
 											<h5 class="font-weight-semibold">Collaborate and Bid</h5>
 											<div>
@@ -182,6 +313,14 @@
 											</div>
 											<p class="mt-2 mb-0 text-muted">Select Partners who will join you in the project</p>
 										</div>
+									</div> -->
+								</div>
+							</div>
+							<div class="row">
+								<div class="col-lg-12">
+									<div class="form-group">
+										<label class="d-block"><strong>Description</strong> (Max 2000 chars)</label>
+										<textarea rows="5" cols="5" class="form-control" placeholder="Enter your message here"></textarea>
 									</div>
 								</div>
 							</div>
